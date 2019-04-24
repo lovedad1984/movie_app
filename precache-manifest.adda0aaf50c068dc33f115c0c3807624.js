37a505649d5498b815b4c1f65a852edde76f4104
Song Bong <sm841004@gmail.com>
2019-04-24 17:41:15 +0900

    Updates

---
+++ precache-manifest.adda0aaf50c068dc33f115c0c3807624.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.e6fc6960.js"
+  },
+  {
+    "revision": "32be87ab251a4dce3e3b",
+    "url": "/movie_app/static/js/main.4cb2f6df.chunk.js"
+  },
+  {
+    "revision": "026da97c9cf2fe34d3df",
+    "url": "/movie_app/static/js/2.0618d762.chunk.js"
+  },
+  {
+    "revision": "32be87ab251a4dce3e3b",
+    "url": "/movie_app/static/css/main.46ada787.chunk.css"
+  },
+  {
+    "revision": "2716254b47ad299df03bbd283539a824",
+    "url": "/movie_app/index.html"
+  }
+];
